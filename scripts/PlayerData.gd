f7690b3c8cc160e1a116987eec1cf285c8010365
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2026-05-01 11:24:23 -0300

    refactor(data): remove skill_points e dash_cd_upgrades de PlayerData

--- scripts/PlayerData.gd
+++ scripts/PlayerData.gd
@@ -3,10 +3,8 @@ class_name PlayerData
 
 @export var level: int = 1
 @export var current_xp: float = 0.0
-@export var skill_points: int = 0
 @export var attack_damage_upgrades: int = 0
 @export var skill_damage_upgrades: int = 0
 @export var speed_upgrades: int = 0
 @export var max_health_upgrades: int = 0
-@export var dash_cd_upgrades: int = 0
 @export var soul_fragments: int = 0
